0db84b8b76ecb9bcbe33e8ca655d8581b9359a95
Loey <0brigadeiro1213@gmail.com>
2022-10-15 16:15:48 -0300

    aula sobre laço de repetição

--- 02/jogo_adivinhacao.py
+++ 02/jogo_adivinhacao.py
@@ -3,14 +3,17 @@ print("*********************************")
 print("Bem-vindo no jogo de adivinhação!")
 print("*********************************")
 
-numero_secreto = 43
+numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while (rodada <= total_de_tentativas):
+for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     #relacionados ao chute
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número de 1 a 100: "))
+
+    if(chute < 1 and chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     #condições
     acertou = chute == numero_secreto
@@ -22,12 +25,11 @@ while (rodada <= total_de_tentativas):
     #lógica
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
-
-    rodada = rodada + 1
 
 print("Fim do jogo!")
