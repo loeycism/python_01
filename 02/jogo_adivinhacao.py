0a0f0823326b3a323f6c4ea866ae8adeb41ab015
Loey <0brigadeiro1213@gmail.com>
2022-10-17 18:34:49 -0300

    random numbers

--- 02/jogo_adivinhacao.py
+++ 02/jogo_adivinhacao.py
@@ -1,10 +1,24 @@
+import random
 
 print("*********************************")
 print("Bem-vindo no jogo de adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
-total_de_tentativas = 3
+numero_secreto = random.randrange(1, 101) #gera um número entr 0.0 e 1.0
+total_de_tentativas = 0
+pontos = 100
+
+print("Qual nível de dificuldade você deseja?")
+print("(1) Fácil    (2) Médio    (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -33,3 +47,4 @@ for rodada in range (1, total_de_tentativas + 1):
             print("Você errou! O seu chute foi menor do que o número secreto.")
 
 print("Fim do jogo!")
+print("O número secreto era: ", numero_secreto)
